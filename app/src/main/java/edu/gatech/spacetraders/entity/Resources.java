5abcbeaebaaa05bc63a749910fe9f27bb67071ac
cler <ozburn.c.a@gmail.com>
2019-02-23 15:26:32 -0500

    Added Resources enum for M6

---
+++ app/src/main/java/edu/gatech/spacetraders/entity/Resources.java
@@ -0,0 +1,17 @@
+package edu.gatech.spacetraders.entity;
+
+public enum Resources {
+    NOSPECIALRESOURCES,
+    MINERALRICH,
+    MINERALPOOR,
+    DESERT,
+    LOTSOFWATER,
+    RICHSOIL,
+    POORSOIL,
+    RICHFAUNA,
+    LIFELESS,
+    WEIRDMUSHROOMS,
+    LOTSOFHERBS,
+    ARTISTIC,
+    WARKLIKE
+}
