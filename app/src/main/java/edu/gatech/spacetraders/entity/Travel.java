c91e9787d14d3b54df5cfbf7a41f7fcbbd37d4cd
Jenny Guzdial <jguzdial6@gatech.edu>
2019-03-25 20:47:01 -0400

    created travel class

---
+++ app/src/main/java/edu/gatech/spacetraders/entity/Travel.java
@@ -0,0 +1,11 @@
+package edu.gatech.spacetraders.entity;
+//this class takes in list of planets already created from universe,
+// takes in ship for fuel and location
+//takes in Player
+//methods are calculatePlanetsInRange (also updates list for recyclerview),
+//method to decrement fuel and update location
+//method to  update current planet info(stringbuilder)
+//method for random encounters(two types: affect cargo and affect credits(maybe change ship))
+public class Travel {
+
+}
