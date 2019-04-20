25adf0cd44112baf9b0f67f14467ab92e960a3e6
Jenny Guzdial <jguzdial6@gatech.edu>
2019-04-20 14:35:24 -0400

    Added cut scene of spaceship after pressing travel

--- app/src/main/java/edu/gatech/spacetraders/views/Loading.java
+++ app/src/main/java/edu/gatech/spacetraders/views/Loading.java
@@ -2,6 +2,7 @@ package edu.gatech.spacetraders.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 
 import edu.gatech.spacetraders.R;
 
@@ -9,10 +10,28 @@ import edu.gatech.spacetraders.R;
  * Created loading screen
  */
 public class Loading extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+
+        CountDownTimer timer = new CountDownTimer(1850, 1000) //10 second Timer
+        {
+            public void onTick(long l)
+            {
+
+            }
+
+            @Override
+            public void onFinish()
+            {
+                kill_activity();
+            };
+        }.start();
+    }
+
+    void kill_activity()
+    {
+        finish();
     }
 }
